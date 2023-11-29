d893a925a370cc344cbb3f4431cff69f52c85997
New <New@LAPTOP-LAEBPE6Q>
2023-11-29 17:52:23 +0200

    Добавьте файлы проекта.

---
+++ Tasks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Виводить числа з консолі
+        Console.Write("12"); //Надала random значення "12"
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("56"); //Надала рандомне значення "56"
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        // Перевіряє чи є числа днем та місяцем
+        bool isValidDate = IsValidDate(number1, number2);
+
+        // Виведе необхідний результат
+        if (isValidDate)
+        {
+            Console.WriteLine("Ці числа можуть бути днем та місяцем.");
+        }
+        else
+        {
+            Console.WriteLine("Ці числа не можуть бути днем та місяцем.");
+            Console.ReadKey();
+        }
+    }
+    static bool IsValidDate(int day, int month)
+    {
+        return (day >= 1 && day <= 31) && (month >= 1 && month <= 12);
+    }
+}
